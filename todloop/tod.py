35a9daca96e03e0cf08af38afb15e7f100a0851b
Yilun Guan <guanyilun@users.noreply.github.com>
2018-11-15 00:15:44 -0500

    Bring in some old routines that might be useful (#8)
    * bring in some old routines that might be useful
    
    * add coincident signal routine

--- todloop/tod.py
+++ todloop/tod.py
@@ -1,6 +1,8 @@
 import moby2
 from moby2.scripting import get_filebase
-from base import Routine
+import numpy as np
+
+from todloop.base import Routine
 
 
 class TODLoader(Routine):
@@ -46,4 +48,31 @@ class TODSelector(Routine):
             self.veto()  # halt subsequent routines
 
 
+class FixOpticalSign(Routine):
+    """A routine that corrects for optical sign"""
+    def __init__(self, input_key="tod_data", output_key="tod_data"):
+        Routine.__init__(self)
+        self._input_key = input_key
+        self._output_key = output_key
+
+    def execute(self):
+        tod_data = self.get_store().get(self._input_key)  # retrieve TOD
+        optical_signs = tod_data.info.array_data['optical_sign']
+        tod_data.data = tod_data.data*optical_signs[:, np.newaxis]
+        self.get_store().set(self._output_key, tod_data)
+
+
+class CalibrateTOD(Routine):
+    """A routine that calibrates from DAQ to W"""
+    def __init__(self, input_key="tod_data", output_key="tod_data"):
+        Routine.__init__(self)
+        self._input_key = input_key
+        self._output_key = output_key
+
+    def execute(self):
+        tod = self.get_store().get(self._input_key)
+        cal = moby2.scripting.get_calibration({'type': 'iv', 'source': 'data'}, tod=tod)
+        cal_mask, cal_val = cal.get_property('cal', det_uid=tod.det_uid)
+        tod.data *= cal_val[:,None]
+        self.get_store().set(self._output_key, tod)
 
